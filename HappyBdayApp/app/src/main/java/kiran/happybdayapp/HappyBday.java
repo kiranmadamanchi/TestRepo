aca4fcd16f2874630df74e8fc1df898ded78bb83
KiranKuma <Madamanchi_KiranKuma@AMER.DELL.COM>
2016-05-07 13:21:48 +0530

    HappyBdayApp

---
+++ HappyBdayApp/app/src/main/java/kiran/happybdayapp/HappyBday.java
@@ -0,0 +1,13 @@
+package kiran.happybdayapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HappyBday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy_bday);
+    }
+}
